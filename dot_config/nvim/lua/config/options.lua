8f1169be70d937f6320274ccf7063ac4dbf0a2c1
RyoIshida <hello.daifuku0511@gmail.com>
2026-09-17 03:01:36 +0900

    options.luaに設定を追加

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,2 +1,12 @@
 -- レジスタをシステムのクリップボードと同期
 vim.opt.clipboard = "unnamedplus"
+
+-- 行番号の設定（絶対行番号と相対行番号を両方有効にすると、現在行のみ絶対行番号で表示されます）
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- 現在行のハイライト
+vim.opt.cursorline = true
+
+-- 現在列のハイライト
+vim.opt.cursorcolumn = true
