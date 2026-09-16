3d116b72c9f4338b6ae474d6aefbb85a31bb1534
RyoIshida <hello.daifuku0511@gmail.com>
2026-09-17 03:05:33 +0900

    telescope.luaの更新

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -1,8 +1,26 @@
 return {
-    'nvim-telescope/telescope.nvim', version = '*',
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        -- optional but recommended
-        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    }
+  "nvim-telescope/telescope.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+  },
+  -- キーバインド定義（押した瞬間に Telescope がロードされる）
+  keys = {
+    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
+    { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
+    { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Find buffers" },
+    { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent files" },
+    { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help tags" },
+    { "<leader>/", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Search current buffer" },
+  },
+  config = function()
+    local telescope = require("telescope")
+    telescope.setup({
+      defaults = {
+        path_display = { "truncate" }, -- パス長表示の最適化
+      },
+    })
+    -- fzf 拡張の読み込み
+    telescope.load_extension("fzf")
+  end,
 }
