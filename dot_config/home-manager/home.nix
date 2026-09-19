83331c11966fbd169007fb53c08b1a1af9e862ec
RyoIshida <hello.daifuku0511@gmail.com>
2026-09-19 18:24:31 +0900

    neovim lspの設定

--- dot_config/home-manager/home.nix
+++ dot_config/home-manager/home.nix
@@ -49,6 +49,7 @@
     gnumake
     gcc
     ripgrep
+    nodejs
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -30,6 +30,8 @@ require("lazy").setup({
     { import = "plugins.oil" },
     { import = "plugins.bufferline" },
     { import = "plugins.lualine" },
+    { import = "plugins.toggleterm" },
+    { import = "plugins.lsp" },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
