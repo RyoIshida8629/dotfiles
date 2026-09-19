0dc45f3fb74418ac0276175d4a8224db2ec292ba
RyoIshida <hello.daifuku0511@gmail.com>
2026-09-19 13:47:20 +0900

    [update] telescope.luaの更新

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -1,10 +1,17 @@
 return {
-    'nvim-telescope/telescope.nvim', version = '*',
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        -- optional but recommended
-        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    },
-    opts = {
-    }
+  'nvim-telescope/telescope.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+  },
+  keys = {
+    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "ファイル検索" },
+    { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "文字列検索" },
+    { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "バッファ検索" },
+    { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "履歴検索" },
+    { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "ヘルプ検索" },
+  },
+  opts = {
+  },
 }
