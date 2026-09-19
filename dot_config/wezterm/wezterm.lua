218b0035375344520616e30360dc87e593d65ed2
RyoIshida <hello.daifuku0511@gmail.com>
2026-09-19 16:16:20 +0900

    weztermの背景色を変更

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -17,4 +17,9 @@ config.use_ime = true -- 日本語入力を有効化
 config.font_size = 12 -- フォントサイズ
 config.window_background_opacity = 0.80 -- 背景を透過
 
+-- カラーパレットで背景色を Solarized Osaka のHEX値に指定
+config.colors = {
+  background = '#00141a',
+}
+
 return config
